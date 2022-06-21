7bdb52f7465ea96d30de5b562291f52ebc033575
Simone Celia <simone.celia@simonecelia.it>
2022-06-21 09:29:26 +0200

    add_cons

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from gtts import gTTS
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, filters, MessageHandler
 
-import constants as cons
+import constants as cn
 
 logging.basicConfig(
 	handlers=[
@@ -25,12 +25,12 @@ logging.basicConfig(
 
 
 async def unknown_command(update: Update, context: CallbackContext):
-	await context.bot.send_message(chat_id=update.effective_chat.id, text=cons.UNKNOWN_COMMAND_RESPONSE)
+	await context.bot.send_message(chat_id=update.effective_chat.id, text=cn.UNKNOWN_COMMAND_RESPONSE)
 
 
 async def random_bestemmia(update: Update, context: CallbackContext):
-	context.args.append(random.choice(cons.MOSCONI_ARRAY))
-	response = requests.get(cons.RANDOM_BESTEMMIA_URL)
+	context.args.append(random.choice(cn.MOSCONI_ARRAY))
+	response = requests.get(cn.RANDOM_BESTEMMIA_URL)
 	json_object = json.loads(response.text)
 	bestemmia = json_object["bestemmia"]
 	await context.bot.send_message(chat_id=update.effective_chat.id, text=bestemmia)
@@ -39,65 +39,65 @@ async def random_bestemmia(update: Update, context: CallbackContext):
 
 
 async def random_meme(update: Update, context: CallbackContext):
-	response = requests.get(cons.RANDOM_MEME_URL)
+	response = requests.get(cn.RANDOM_MEME_URL)
 	json_object = json.loads(response.text)
 	await context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=json_object["url"])
 
 
 async def random_gif(update: Update, context: CallbackContext):
-	response = requests.get(cons.RANDOM_GIF_URL)
+	response = requests.get(cn.RANDOM_GIF_URL)
 	json_object = json.loads(response.text)
 	mp4 = json_object["data"]["images"]["original_mp4"]["mp4"]
 	await context.bot.sendDocument(chat_id=update.effective_chat.id, document=mp4)
 
 
 async def random_taunt(update: Update, context: CallbackContext):
-	taunt_array = cons.DAOC_ARRAY
+	taunt_array = cn.DAOC_ARRAY
 	text = update.message.text
-	if text.startswith('/random_ts'):
-		taunt_array = cons.TS_ARRAY
-	elif text.startswith('/random_aoe'):
-		taunt_array = cons.AOE_ARRAY
-	elif text.startswith('/random_dipre'):
-		taunt_array = cons.DIPRE_ARRAY
-	audio = cons.SIMONECELIA_DATA_URL + random.choice(taunt_array) + cons.MP3
+	if text.startswith(cn.SLASH + cn.RANDOM_TS):
+		taunt_array = cn.TS_ARRAY
+	elif text.startswith(cn.SLASH + cn.RANDOM_AOE):
+		taunt_array = cn.AOE_ARRAY
+	elif text.startswith(cn.SLASH + cn.RANDOM_DIPRE):
+		taunt_array = cn.DIPRE_ARRAY
+	audio = cn.SIMONECELIA_DATA_URL + random.choice(taunt_array) + cn.MP3
 	await context.bot.sendAudio(chat_id=update.effective_chat.id, audio=audio)
 
 
 async def play(update: Update, context: CallbackContext):
 	taunt = ' '.join(context.args).strip()
 	if '' == taunt:
-		await context.bot.send_message(chat_id=update.effective_chat.id, text=cons.ERROR_PARAMETER_NEEDED)
+		await context.bot.send_message(chat_id=update.effective_chat.id, text=cn.ERROR_PARAMETER_NEEDED)
 	else:
-		if taunt in cons.DAOC_ARRAY or taunt in cons.TS_ARRAY or taunt in cons.AOE_ARRAY or taunt in cons.DIPRE_ARRAY:
-			audio = cons.SIMONECELIA_DATA_URL + taunt + cons.MP3
+		if taunt in cn.DAOC_ARRAY or taunt in cn.TS_ARRAY or taunt in cn.AOE_ARRAY or taunt in cn.DIPRE_ARRAY:
+			audio = cn.SIMONECELIA_DATA_URL + taunt + cn.MP3
 			await context.bot.sendAudio(chat_id=update.effective_chat.id, audio=audio)
 		else:
-			await context.bot.send_message(chat_id=update.effective_chat.id, text=cons.TAUNT_NOT_FOUND)
+			await context.bot.send_message(chat_id=update.effective_chat.id, text=cn.TAUNT_NOT_FOUND)
 			logging.error("Taunt not found, input text = " + taunt)
 
 
 async def list_play(update: Update, context: CallbackContext):
-	await context.bot.send_message(chat_id=update.effective_chat.id, text=cons.TS_BOT_WEB_LINK)
+	await context.bot.send_message(chat_id=update.effective_chat.id, text=cn.TS_BOT_WEB_LINK)
 
 
 async def tts(update: Update, context: CallbackContext, bestemmia=''):
-	language = cons.IT
+	language = cn.IT
 	if '' != bestemmia:
 		text = bestemmia
 	else:
 		cmd = update.message.text
-		if cmd.startswith('/tts_es'):
-			language = cons.ES
-		elif cmd.startswith('/tts_en'):
-			language = cons.EN
+		if cmd.startswith(cn.SLASH + cn.TTS_ES):
+			language = cn.ES
+		elif cmd.startswith(cn.SLASH + cn.TTS_EN):
+			language = cn.EN
 		text = ' '.join(context.args).strip()
 	if '' == text:
-		await context.bot.send_message(chat_id=update.effective_chat.id, text=cons.ERROR_PARAMETER_NEEDED)
+		await context.bot.send_message(chat_id=update.effective_chat.id, text=cn.ERROR_PARAMETER_NEEDED)
 	else:
 		myobj = gTTS(text=text, lang=language, slow=False)
-		myobj.save(cons.MP3_TEMP_FILE)
-		await context.bot.sendAudio(chat_id=update.effective_chat.id, audio=open(cons.MP3_TEMP_FILE, "rb"))
+		myobj.save(cn.MP3_TEMP_FILE)
+		await context.bot.sendAudio(chat_id=update.effective_chat.id, audio=open(cn.MP3_TEMP_FILE, "rb"))
 
 
 def get_version():
@@ -107,14 +107,14 @@ def get_version():
 
 
 if __name__ == '__main__':
-	application = ApplicationBuilder().token(cons.TOKEN).build()
+	application = ApplicationBuilder().token(cn.TOKEN).build()
 	application.add_handler(CommandHandler('random_bestemmia', random_bestemmia))
 	application.add_handler(CommandHandler('random_meme', random_meme))
 	application.add_handler(CommandHandler('random_gif', random_gif))
-	application.add_handler(CommandHandler(['random_daoc', 'random_ts', 'random_aoe', 'random_dipre'], random_taunt))
+	application.add_handler(CommandHandler(['random_daoc', cn.RANDOM_TS, cn.RANDOM_AOE, cn.RANDOM_DIPRE], random_taunt))
 	application.add_handler(CommandHandler('play', play))
 	application.add_handler(CommandHandler('list_play', list_play))
-	application.add_handler(CommandHandler(['tts_en', 'tts_es', 'tts_it'], tts))
+	application.add_handler(CommandHandler([cn.TTS_EN, cn.TTS_ES, cn.TTS_IT], tts))
 	application.add_handler(MessageHandler(filters.COMMAND, unknown_command))
 	get_version()
 	application.run_polling(stop_signals=None)
