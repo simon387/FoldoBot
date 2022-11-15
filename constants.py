a9dc878105b853e26c1050f6f477f41876cea0fb
simone.celia <simone.celia@eng.it>
2022-11-15 09:15:47 +0100

    1.3.1

--- constants.py
+++ constants.py
@@ -12,7 +12,7 @@ GIPHY_KEY = config.get(SECRETS, 'api.giphy.com.key')
 UNKNOWN_COMMAND_RESPONSE = "Questo comando non esite!"
 STARTUP_MESSAGE = "FoldoBot started! "
 SHUTDOWN_MESSAGE = "FoldoBot stopped!"
-RANDOM_BESTEMMIA_URL = "https://www.simonecelia.it/random_bestemmia.php"
+RANDOM_BESTEMMIA_URL = "https://bestemmie.org/api/random/"
 RANDOM_MEME_URL = "https://meme-api.herokuapp.com/gimme"
 RANDOM_GIF_URL = "https://api.giphy.com/v1/gifs/random?api_key=" + GIPHY_KEY + "&rating=pg-13&limit=1"
 TS_BOT_WEB_DATA_URL = "www.simonecelia.it/ts-bot-web/data/"
